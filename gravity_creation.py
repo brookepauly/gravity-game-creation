5cb611c3caa188b2b86f79deaaa2989fd8033988
brookepauly <bpauly2003@gmail.com>
2026-03-22 20:13:15 -0400

    Initial Screen/Quit Setup

--- gravity_creation.py
+++ gravity_creation.py
@@ -1,1 +1,17 @@
 import pygame 
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
