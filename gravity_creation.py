5e31a8ab185bc20f331514957f14ec9c52aada05
brookepauly <bpauly2003@gmail.com>
2026-03-22 21:43:47 -0400

    Add Git And Vocab File

--- gravity_creation.py
+++ gravity_creation.py
@@ -1,9 +1,23 @@
 import pygame 
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env file
+file_url = os.getenv('FILE_URL')
+
+vocab = pd.read_csv(file_url)
 
 pygame.init()
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
+
+score_info = pd.DataFrame({
+    'Points': [],
+    'Correct': [],
+    'Incorrect': []
+})
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -12,6 +26,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.fill((255, 255, 255))
 
 pygame.quit()
 
